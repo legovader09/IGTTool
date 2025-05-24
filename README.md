37d8f4b6c9cb6635779f930bb4e685941aa667b0
Dominik Hauerstein <dcdominoes@gmail.com>
2025-05-24 19:47:03 +0100

    Initialize typescript

---
+++ README.md
@@ -0,0 +1,19 @@
+## Minimal React Template
+### Description:
+A Reactjs template transpiled with babel and bundled via webpack.
+
+### Features
+- React TS
+- Sass
+- Prop-types
+
+### Requirements
+This project uses **Node 18.20.4**.
+
+### Steps
+This project uses 
+1. Download or fork this repository.
+2. Run `npm install`.
+3. Start creating!
+
+A quick preview of the base site can be found [here](https://legovader09.github.io/ReactTemplate/).
---
+++ webpack.config.base.js
@@ -0,0 +1,81 @@
+const path = require('path');
+const webpack = require('webpack');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('@soda/friendly-errors-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: {
+    app: ['./src/js/index.tsx', './src/scss/index.scss'],
+  },
+  target: ['web', 'es5'],
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new Dotenv({
+      path: './.env',
+      safe: true,
+      systemvars: true,
+    }),
+    new ESLintPlugin({ emitError: true, failOnError: true }),
+    new FriendlyErrorsWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: './css/bundle.css' }),
+    new CopyPlugin({ patterns: [{ from: './public', to: path.resolve(__dirname, 'build') }] }),
+    new CompressionPlugin({
+      algorithm: 'gzip',
+      test: /\.js(\?.*)?$/i,
+    }),
+  ],
+  output: {
+    filename: './js/bundle.js',
+    path: path.join(__dirname, 'build'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(html|png|jpg|jpeg|svg|ttf|otf|webp)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader, // extract CSS into files
+          {
+            loader: 'css-loader', // turns CSS into CommonJS
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                parser: 'postcss-scss',
+                plugins: ['postcss-preset-env'],
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              api: 'modern',
+              sassOptions: {
+                fiber: false,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.scss', '.ts', '.tsx'],
+  },
+};
